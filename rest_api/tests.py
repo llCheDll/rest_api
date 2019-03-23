50193384b6851b7970e2873e532046809ee2c79e
llCheDll <cherniak.d.v@gmail.com>
2019-03-23 11:03:33 +0000

    [master]Start testing

--- rest_api/tests.py
+++ rest_api/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import RequestFactory, TestCase, Client
+from rest_api.views import RegisterView
+from rest_api.urls import TokenObtainPairView
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        # self.factory = RequestFactory()
+        # self.user = User.objects.create_user(
+        #     username='mmitch', email='mmitch@gmail.com', password='1234')
+        # import ipdb
+        # ipdb.set_trace()
+
+    def test_signup(self):
+        data = {
+            'email': 'mmitch@gmail.com',
+            'username': 'mmitch',
+            'password': '1234'
+        }
+
+        response = self.client.post(
+            '/api/v1/signup/',
+            data,
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {'status': 'user_create'})
+
+    def test_login(self):
+        import ipdb
+        ipdb.set_trace()
+        # request = self.client.post(
+        #     '/api/v1/login/',
+        #     data,
+        #     content_type='application/json'
+        # )
+
+        # request.user = AnonymousUser()
+        # response = TokenObtainPairView.as_view()(request)
+
+        # token_names = ['refresh', 'access']
+
+        # self.assertEqual(response.status_code, 200)
+        # self.assertEqual([name for name in response.data.keys()], token_names)
+
+        # self.user.token = response.data['access']
+
+
